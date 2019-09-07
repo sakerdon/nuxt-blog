051ef8bbfa580bf75ff197ed7cfae02b6d77a30a
Ivan Nizhegorodov <inizegorodov@gmail.com>
2019-09-07 12:28:34 +0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ module.exports = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+    baseUrl: process.env.BASE_URL /*|| 'http://localhost:3000'*/
   },
 
   env: {
