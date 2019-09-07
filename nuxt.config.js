34d6967912714f42ba6184c359779a57153951e9
Ivan Nizhegorodov <inizegorodov@gmail.com>
2019-09-07 12:35:42 +0300

    change url

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,8 @@ module.exports = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseUrl: process.env.BASE_URL /*|| 'http://localhost:3000'*/
+    // baseUrl: process.env.BASE_URL /*|| 'http://localhost:3000'*/
+    baseUrl: 'https://nuxt-bestblog.herokuapp.com/'
   },
 
   env: {
